ee768b82ed46d57ecbb4c4ea0eb367e20572a705
bigmurry <hhdiao@siom.ac.cn>
2016-11-21 23:33:33 +0800

    make less then 10 lines

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-module.exports = function (end) {
-  return {
-    *[Symbol.iterator] () {
-      let [pre, cur] = [1, 0]
-      while (cur < end) {
-        yield cur;
-        [pre, cur] = [cur, pre + cur]
-      }
+module.exports = (end) => ({
+  *[Symbol.iterator] () {
+    let [pre, cur] = [1, 0]
+    while (cur < end) {
+      yield cur;
+      [pre, cur] = [cur, pre + cur]
     }
   }
-}
+})
